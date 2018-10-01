c11170428f67a8c1db7c82cfd03dc6db40f7fd5d
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-09-30 21:37:08 -0400

    Expanded to blog model and tested with script

--- blog/src/script.ts
+++ blog/src/script.ts
@@ -2,10 +2,15 @@ const { prisma } = require('./generated/prisma-client')
 
 // A `main` function so that we can use async/await
 async function main() {
-
-  // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: 'Alice' })
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+  // create a new user
+  const newUser = await prisma.createUser({
+    email:      "foo@gmail.com",
+    hash:       "slkdjf",
+    firstName:  "Alice",
+    lastName:   "Smith",
+    posts: {create: []},
+  })
+  console.log(`New User ${JSON.stringify(newUser)}`)
 
   // Read all users from the database and print them to the console
   const allUsers = await prisma.users()
