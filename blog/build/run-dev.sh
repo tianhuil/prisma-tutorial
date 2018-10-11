888ba93a11fede2b2374cc656808664dbbe23c57
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-10-11 13:36:53 -0400

    Build with environment variables

---
+++ blog/build/run-dev.sh
@@ -0,0 +1,13 @@
+PRISMA_CONFIG=$'
+port: 4466
+databases:
+  default:
+    connector: postgres
+    host: postgres
+    port: 5432
+    user: prisma
+    password: prisma
+    migrations: true
+'
+
+NETWORK_NAME=blog-network-dev PRISMA_CONFIG=${PRISMA_CONFIG} docker-compose up
