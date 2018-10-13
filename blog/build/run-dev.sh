dbff5f80c0c3721260677bf441645f8a1a434939
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-10-13 13:16:07 -0400

    prisma_config.yml

--- blog/build/run-dev.sh
+++ blog/build/run-dev.sh
@@ -1,13 +1,3 @@
-PRISMA_CONFIG=$'
-port: 4466
-databases:
-  default:
-    connector: postgres
-    host: postgres
-    port: 5432
-    user: prisma
-    password: prisma
-    migrations: true
-'
-
-NETWORK_NAME=blog-network-dev PRISMA_CONFIG=${PRISMA_CONFIG} docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
+export NETWORK_NAME=blog-network-dev
+export PRISMA_CONFIG=`cat build/prisma_config.yml`
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
