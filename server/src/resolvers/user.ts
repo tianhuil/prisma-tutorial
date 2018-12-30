a38b3458854e2b5841fb9e95c6dd68c5daf0886b
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-12-29 23:00:55 -0800

    parent{->Fn}

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,18 +1,18 @@
 import { forwardTo } from 'prisma-binding'
 
-function parent (parent, args, ctx, info) {
+function parentFn(parent, args, ctx, info) {
   return parent[info.fieldName]
 }
 
 export const UserTypes = {
   PublicUser: {
-    id:         parent,
-    firstName:  parent,
-    lastName:   parent,
-    about:      parent,
-    createdAt:  parent,
-    updatedAt:  parent,
-    posts:      parent,
+    id:         parentFn,
+    firstName:  parentFn,
+    lastName:   parentFn,
+    about:      parentFn,
+    createdAt:  parentFn,
+    updatedAt:  parentFn,
+    posts:      parentFn,
   }
 }
 
